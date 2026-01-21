a39313a0e949bdc458e74e78edfa5d9aa6913b22
bipin <bipin.alt.ev7@gmail.com>
2026-01-22 02:07:45 +0545

    File formating and Creating slices

---
+++ miniBlogAppWithAuthAndRedux/Shadcn-pnpm-React_tailwind-StarterKit/src/components/AddBlog.jsx
@@ -0,0 +1,9 @@
+function AddNewBlog() {
+    return ( 
+        <div>
+            <h1> This is  add new Blog Component</h1>
+        </div>
+     );
+}
+
+export default AddNewBlog;
---
+++ miniBlogAppWithAuthAndRedux/Shadcn-pnpm-React_tailwind-StarterKit/src/components/BlogList.jsx
@@ -0,0 +1,9 @@
+function BlogList() {
+    return ( 
+     <div>
+        <h1>This is the BlogList Component.</h1>
+     </div>
+     );
+}
+
+export default BlogList;
